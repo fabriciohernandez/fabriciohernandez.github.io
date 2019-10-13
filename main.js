b6b542f82ec40112323534bc10f839662d77f97f
Fabrcio Hernandez <00163217@uca.edu.sv>
2019-10-12 18:19:09 -0600

    FINISH Informacion y cambio de dia

--- main.js
+++ main.js
@@ -20,8 +20,25 @@ function ocultar(elemento){
     }
 }
 
-lunes.addEventListener('click',function(){
 
+function mostrar(elemento){
+    for (var i=0;i<elemento.length;i++){
+        elemento[i].style.display='flex';
+    }
+}
+
+//------OCULTANDO TODOS LOS ELEMENTOS--------
+ocultar(lunesCard);
+ocultar(martesCard);
+ocultar(miercolesCard);
+ocultar(juevesCard);
+ocultar(viernesCard);
+//------OCULTANDO TODOS LOS ELEMENTOS--------
+
+
+
+lunes.addEventListener('click',function(){
+    mostrar(lunesCard);
     ocultar(martesCard);
     ocultar(miercolesCard);
     ocultar(juevesCard);
@@ -35,6 +52,7 @@ lunes.addEventListener('click',function(){
 
 martes.addEventListener('click',function(){
     ocultar(lunesCard);
+    mostrar(martesCard);
     ocultar(miercolesCard);
     ocultar(juevesCard);
     ocultar(viernesCard);
@@ -42,66 +60,40 @@ martes.addEventListener('click',function(){
     console.log('Le diste click a martes');
     var dia = document.querySelector('#dia');
     dia.textContent = 'Martes despues del inicio de semana';
- 
 
-    //seteando informacion de la carta 2
-    var titulo2 = document.querySelector('#title2');
-    titulo2.textContent = '';
-    var info2 = document.querySelector('#info-2');
-    info2.textContent = 'Seguir avanzando con el projecto, ahora con mis companeros terminamos de disenar el modelo de la base de datos.';
-
-    //seteando imagen de social media
 })
 miercoles.addEventListener('click',function(){
+    ocultar(lunesCard);
+    ocultar(martesCard);
+    mostrar(miercolesCard);
+    ocultar(juevesCard);
+    ocultar(viernesCard);
+
     console.log('Le diste click a miercoles');
     var dia = document.querySelector('#dia');
-    dia.textContent = 'Miercoles Vamos por la mitad!';
-    //Seteando informacion de la carta 1
-    var titulo1 = document.querySelector('#title1');
-    titulo1.textContent = 'Cena Familiar';
-    var info1 = document.querySelector('#info-1');
-    info1.textContent = 'Este dia a pesar de su respectivo estres, termina de maravilla en casa junto con la familia.';
-
-    //seteando informacion de la carta 2
-    var titulo2 = document.querySelector('#title2');
-    titulo2.textContent = 'Que siga el trabajo';
-    var info2 = document.querySelector('#info-2');
-    info2.textContent = 'El avance de este dia fue que terminamos de hacer el prototipo de la aplicacion de escritorio.';
+    dia.textContent = '¡Miercoles Vamos por la mitad!';
 
 })
 jueves.addEventListener('click',function(){
-    console.log('Le diste click a jueves');
+    ocultar(lunesCard);
+    ocultar(martesCard);
+    ocultar(miercolesCard);
+    mostrar(juevesCard);
+    ocultar(viernesCard);
 
+    console.log('Le diste click a jueves');
     var dia = document.querySelector('#dia');
     dia.textContent = 'Jueves de amigos';
-    //Seteando informacion de la carta 1
-    var titulo1 = document.querySelector('#title1');
-    titulo1.textContent = 'Projecto vamos avanzando';
-    var info1 = document.querySelector('#info-1');
-    info1.textContent = 'A pesar de ser un projecto bastante grande seguimos avanzando, este dia terminamos el diseno del login. :)';
-
-    //seteando informacion de la carta 2
-    var titulo2 = document.querySelector('#title2');
-    titulo2.textContent = 'Estudiar';
-    var info2 = document.querySelector('#info-2');
-    info2.textContent = 'El dia del examen se acerca y todavia no me siento listo...';
-
 })
 viernes.addEventListener('click',function(){
+    ocultar(lunesCard);
+    ocultar(martesCard);
+    ocultar(miercolesCard);
+    ocultar(juevesCard);
+    mostrar(viernesCard);
     console.log('Le diste click a viernes');
 
     var dia = document.querySelector('#dia');
-    dia.textContent = 'Al fin Viernes!';
-    //Seteando informacion de la carta 1
-    var titulo1 = document.querySelector('#title1');
-    titulo1.textContent = 'Cumpleanos de mi madre';
-    var info1 = document.querySelector('#info-1');
-    info1.textContent = 'Hoy 11 de ocubre nace mi madre, asi que sera un dia de celebracion.';
-
-    //seteando informacion de la carta 2
-    var titulo2 = document.querySelector('#title2');
-    titulo2.textContent = 'Dia del examen! :o';
-    var info2 = document.querySelector('#info-2');
-    info2.textContent = 'Dia del examen, tengo miedo pero he estudiado.';
+    dia.textContent = '¡Al fin Viernes!';
 
 })
